34078518babe28a501bd8546f04145bf5ae221af
Mohammed <jattoe3@myumanitoba.ca>
2025-03-16 01:27:02 -0500

    Updated javafile

--- src/main/java/com/studentapp/NumberGuessServlet.java
+++ src/main/java/com/studentapp/NumberGuessServlet.java
@@ -46,5 +46,7 @@ public class NumberGuessServlet extends HttpServlet {
         }
         out.println("<a href='guess'>Play Again</a>");
     }
-
+    public int getTargetNumber() {
+        return targetNumber;
+    }
 }
